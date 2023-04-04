639299dd9d31caff5eed61ea171c9ba740bcd5e7
Adam Severson <46274806+adamsevo@users.noreply.github.com>
2023-04-04 11:52:43 -0500

    Testing 500 error

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ def index():
 
 @app.route('/about', methods=["GET","POST"])
 def about():
-    return render_template(
+    return rexder_template(
         "about.html", pageTitle="About", friends=friends_dict
     )
 
@@ -82,4 +82,4 @@ def add():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False)
